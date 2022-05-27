9adffea77d8cf949ae375903d9da2ce199d134e2
Aaron Steichen <aaron.steichen@fishtownanalytics.com>
2022-05-27 16:49:27 +0000

    added inc and snap

--- models/marts/core/fct_squid_inkcremental.sql
+++ models/marts/core/fct_squid_inkcremental.sql
@@ -1,13 +1,24 @@
 {{
-    config(
-        materialized='incremental',
-        unique_key='id',
-        incremental_strategy='merge',
-        on_schema_change='append_new_columns'
-    )
+    config(
+        materialized='incremental',
+        unique_key='id',
+        incremental_strategy='merge',
+        on_schema_change='append_new_columns'
+    )
 }}
 
-select * from {{source('produce','squid_inc')}}
+select 
+ID,
+EVENT_TIME,
+SQUID_NAME,
+SUFFIX,
+INK_COLOR,
+INK_PRODUCED_OZ,
+NOTES
+from
+
+{{source('produce','squid_inc')}}
+
 {% if is_incremental() %}
 where event_time > (select max(event_time) from {{ this }})
 {% endif %}
