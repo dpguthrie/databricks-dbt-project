25c1c5656297d8a54f2d9a8d79dcc6a0d3106870
Aaron Steichen <aaron.steichen@fishtownanalytics.com>
2022-06-06 21:47:02 +0000

    added region column to agg orders by month

--- models/marts/aggregates/agg_orders_by_month.sql
+++ models/marts/aggregates/agg_orders_by_month.sql
@@ -1,8 +1,9 @@
 select 
 sum(gross_item_sales_amount) as gross_sales,
-trunc(order_date, 'month') as order_month
+trunc(order_date, 'month') as order_month,
+region
 from {{ ref('dim_customers') }} c
 join {{ ref('fct_orders')}} o 
 on o.customer_key = c.customer_key
-group by order_month 
+group by order_month , region
 order by order_month desc
