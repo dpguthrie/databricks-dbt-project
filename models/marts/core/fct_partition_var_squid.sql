5eb84a9c0d7e1b5390c0dde8b757c2c8b94a4b35
Aaron Steichen <aaron.steichen@fishtownanalytics.com>
2022-09-28 18:36:11 +0000

    more python tests

---
+++ models/marts/core/fct_partition_var_squid.sql
@@ -0,0 +1,18 @@
+{{
+config(
+materialized='incremental',
+unique_key='id',
+incremental_strategy='merge',
+location_root = '/dbfs/delta/external_tables',
+partition_by = 'ID'
+)
+}}
+
+
+select *  from {{ source('produce', 'dupeset') }}
+
+where ID like '{{ var("partition_squid_id") }}' 
+
+{% if is_incremental() %}
+and event_time > (select max(event_time) from {{ this }})
+{% endif %}
