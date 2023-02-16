4cf3c545cc49f451c61c37a3d0799a06c97917fa
Aaron Steichen <aaron.steichen@fishtownanalytics.com>
2023-02-16 21:37:54 +0000

    added region

--- models/marts/aggregates/agg_orders_by_month.sql
+++ models/marts/aggregates/agg_orders_by_month.sql
@@ -1,9 +1,9 @@
 select 
 sum(gross_item_sales_amount) as gross_sales,
 trunc(order_date, 'month') as order_month
--- , region
+ , region
 from {{ ref('dim_customers') }} c
 join {{ ref('fct_orders')}} o 
 on o.customer_key = c.customer_key
-group by order_month -- , region
+group by order_month  , region
 order by order_month desc
